46d95e9990a57a0959fe571acc7e2a0532c35fcd
ValentynNovikov <walian19947@gmail.com>
2017-03-24 15:15:03 +0200

    add folders

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,30 @@ gulp.task('inject', function () {
 		sources = gulp.src(['app/js/**/*.js', 'app/assets/styles/styles/**/*.css'], {read: false});
 
 	return target.pipe(inject(sources,{ relative: true }))
-		.pipe(gulp.dest('./'))
+		.pipe(gulp.dest('dist'))
 		.pipe(browserSync.stream());
+});
+
+gulp.task("build-js", function () {
+	return gulp.src("app/js/**/*.js")
+		.pipe(sourcemaps.init())
+		.pipe(babel())
+		.pipe(concat("all.js"))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest("./dist"))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('watch', function(){
+	gulp.watch('app/js/**/*.js',['build-js']);
+	gulp.watch('app/assets/styles/**/*.css',['index']);
+	gulp.watch("index.html").on('change', browserSync.reload);
+});
+
+gulp.task('serve',['watch','inject'], function() {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	})
 });
