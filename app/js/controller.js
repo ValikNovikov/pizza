5dc074912182cafe5c34937bb2d7c5626b997f19
ValentynNovikov <walian19947@gmail.com>
2017-03-28 16:26:24 +0300

    #7  add new menu

--- app/js/controller.js
+++ app/js/controller.js
@@ -12,3 +12,21 @@ $(function () {
 
 	$('.single-item').slick();
 });
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+	$("#mySidenav").css({'width':'290px'});
+}
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+	$("#mySidenav").css({'width':'0'});
+}
+
+function showMobileDropDown () {
+	if($('.mobile-dropdown').css('display') == 'none'){
+		$('.mobile-dropdown').show('slow');
+	} else {
+		$('.mobile-dropdown').hide('slow');
+	}
+
+}
