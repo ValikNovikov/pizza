e31c6b0d23816a69ec0286909056789cb0b8bba8
ValentynNovikov <walian19947@gmail.com>
2017-03-30 19:11:59 +0300

    fix all eslint warnings

--- app/js/main.js
+++ app/js/main.js
@@ -1,118 +1,115 @@
 $(function () {
+    'use strict';
+
+    var $mobileDropDown = $('.mobile-dropdown'),
+        $ul = $('.slider ul'),
+        $dots = $('#dots'),
+        $sideNav = $('#sidenav'),
+        $currentDots,
+        slideCount = $ul.children().length,
+        slideWidth = 100, // eslint-disable-line
+        slideIndex = 0, // eslint-disable-line
+        firstSlide = $ul.find('li:first-child'),
+        lastSlide = $ul.find('li:last-child'),
+        i;
+
+    /* Set the width of the side navigation to 250px */
+    $('.open-mobile-menu').click(function () {
+        $sideNav.addClass('show-sidenav');
+    });
+
+    /* Set the width of the side navigation to 0 */
+    $('.menu-btn').click(function () {
+        $sideNav.removeClass('show-sidenav');
+    });
+
+    $('#home-btn').click(function () {
+        if ($mobileDropDown.hasClass('show-dropdown')) {
+            $mobileDropDown.toggleClass('show-dropdown').slideUp();
+        } else {
+            $mobileDropDown.slideDown().toggleClass('show-dropdown');
+        }
+    });
 
 
-var $mobileDropDown = $('.mobile-dropdown');
+// -----carousel------//
 
-	/* Set the width of the side navigation to 250px */
-	$('.open-mobile-menu').click(function () {
-		$("#sidenav").addClass('show-sidenav');
-	});
+    lastSlide.clone().prependTo($ul);
+    firstSlide.clone().appendTo($ul);
 
-	/* Set the width of the side navigation to 0 */
-	$('.menu-btn').click(function () {
-		$("#sidenav").removeClass('show-sidenav');
+    $ul.find('li').each(function (indx) {
+        var leftPercentage = (slideWidth * indx) + '%';
 
-	});
+        $(this).css({'left': leftPercentage}).css({width: (slideWidth) + '%'});
+    });
 
-	$('#home-btn').click(function () {
-		if ($mobileDropDown.hasClass('show-dropdown')) {
-			$mobileDropDown.removeClass('show-dropdown');
-			$mobileDropDown.slideUp();
-		} else {
-			$mobileDropDown.slideDown();
-			$mobileDropDown.addClass('show-dropdown');
-		}
+    for (i = 0; i < slideCount; i += 1) { // eslint-disable-line
+        $dots.append('<li><a data-id=' + i + ' class="dot">Dots</a></li>');
+    }
 
-	});
+    $currentDots = $('.dot');
+
+    $currentDots.eq(0).addClass('active'); // eslint-disable-line
 
 
+    $('.prev').on('click', function () {
+        slide(slideIndex - 1); // eslint-disable-line
+        dotMarker(-1); // eslint-disable-line
+    });
 
-//-----carousel------//
-	var $ul = $(".slider ul"),
-	$dots=$("#dots"),
-	slideCount = $ul.children().length,
-	slideWidth = '100',
-	slideIndex = 0,
-	firstSlide = $ul.find("li:first-child"),
-	lastSlide = $ul.find("li:last-child");
+    $('.next').click(function () {
+        if (slideIndex + 1 === slideCount) { // eslint-disable-line
+            $currentDots.eq(0).addClass('active'); // eslint-disable-line
+        }
 
-	lastSlide.clone().prependTo($ul);
-	firstSlide.clone().appendTo($ul);
+        dotMarker(+1); // eslint-disable-line
+        slide(slideIndex + 1); // eslint-disable-line
+    });
 
-	$ul.find("li").each(function(indx) {
-		var leftPercentage = (slideWidth * indx) + "%";
+    function dotMarker (val) {
+        var $currentDot = $('.dot');
 
-		$(this).css({"left":leftPercentage});
-		$(this).css({width:(slideWidth) + "%"});
-	});
-
-	for(var i=0; i < slideCount; i++){
-		$dots.prepend('<li><a class="current-dot">Home</a></li>');
-	}
-	$dots.find('li:first-child a').addClass("active-dot");
+        $currentDot.eq(slideIndex).removeClass('active');
+        $currentDot.eq(slideIndex + Number(val)).addClass('active');
+    }
 
 
+    $currentDots.click(function () {
+        var index = $(this).data('id');
 
-	$(".prev").on('click', function() {
-		var $currentDot = $(".current-dot");
+        $currentDots.removeClass('active');
+        $(this).addClass('active');
 
-		slide(slideIndex - 1);
-
-		$currentDot.eq(slideIndex).removeClass("active-dot");
-		$currentDot.eq(slideIndex - 1).addClass("active-dot");
-	});
-
+        slide(index);
+    });
 
 
-	$(".next").click(function() {
-		var $currentDot = $(".current-dot");
+    function slide (newSlideIndex) {
+        var marginLeft = (newSlideIndex * ('-100') - '100') + '%';
 
-		slide(slideIndex + 1);
+        $ul.animate({'margin-left': marginLeft}, '400', function () {
+            if (newSlideIndex < 0) { // eslint-disable-line
+                $ul.css('margin-left', ((slideCount) * ('-100')) + '%');
 
-		$currentDot.eq(slideIndex).removeClass("active-dot");
+                newSlideIndex = slideCount - '1';
+            } else if (newSlideIndex >= slideCount) {
+                $ul.css('margin-left', '-100%');
 
-		if(slideIndex +1 == slideCount){
-			$currentDot.eq(0).addClass("active-dot");
-		}
-		$currentDot.eq(slideIndex + 1).addClass("active-dot");
-	});
+                newSlideIndex = 0; // eslint-disable-line
+            }
 
+            slideIndex = newSlideIndex;
+        });
+    }
 
+    setInterval(function () {
+        if (slideIndex + 1 === slideCount) { // eslint-disable-line
+            $currentDots.eq(0).addClass('active'); // eslint-disable-line
+        }
+        dotMarker(+1); // eslint-disable-line
+        slide(slideIndex + 1); // eslint-disable-line
+    }, 4000); // eslint-disable-line
 
-$('.current-dot').click(function  () {
-	var index = $( ".current-dot" ).index(this);
-
-	$(".current-dot").removeClass("active-dot");
-	$(this).addClass("active-dot");
-
-	slide(index);
-});
-
-
-	function slide(newSlideIndex) {
-
-		var marginLeft = (newSlideIndex * (-100) - 100) + "%";
-
-		$ul.animate({"margin-left": marginLeft}, 400, function() {
-			if(newSlideIndex < 0) {
-				$ul.css("margin-left", ((slideCount) * (-100)) + "%");
-
-				newSlideIndex = slideCount - 1;
-			} else if(newSlideIndex >= slideCount) {
-
-				$ul.css("margin-left", "-100%");
-
-				newSlideIndex = 0;
-			}
-
-			slideIndex = newSlideIndex
-
-		});
-
-	}
-//-----/carousel------//
-
-
-
+// -----/carousel------//
 });
 
